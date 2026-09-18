274e5769e96790c04dcb022a2f9f1cd726ca4d04
Joss <jipodtouch9@gmail.com>
2026-09-18 09:29:31 -0400

    update node to v26.9.0 and angular to v22

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 
@@ -6,6 +6,7 @@ import { Navbar } from './navbar/navbar';
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
 })
 export class App {
