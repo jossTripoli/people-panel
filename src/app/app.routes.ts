2e9bd1bf1665a8e6dd328d86205e72b2acbe97e4
Joss <jipodtouch9@gmail.com>
2026-09-18 11:04:16 -0400

    Users page component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Users } from './users/users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    component: Users,
+  },
+];
